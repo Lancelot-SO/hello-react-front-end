2724a014c332eafbe9449fe9922548776ba90099
Lancelot-SO <fsowah001@gmail.com>
2023-06-07 17:40:11 +0000

    add project setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Greeting from './components/Greeting';
 
 function App() {
   return (
-    <h1>Hello React</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Greeting />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
